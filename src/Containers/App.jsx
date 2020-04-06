51ba90526d6803d07f8d59fd603f3d3bb4397aa8
Luis <33837673+lfresnog@users.noreply.github.com>
2020-04-06 19:18:37 +0200

    Add files via upload

--- src/Containers/App.jsx
+++ src/Containers/App.jsx
@@ -1,27 +1,8 @@
 import React, { Component } from 'react';
-import cloneDeep from "clone-deep";
 import './App.css';
 import Locations from '../Components/Locations'
 
 class App extends Component {
-  state={
-    location:"",
-    coordinates:""
-  }
-
-  locationHandler = (location) =>{
-    let loc = cloneDeep(this.state);
-    loc = location;
-    this.setState({location:loc});
-    this.setState({coordinates:""});
-    console.log(loc);
-}
-
-coordinatesHandler = (coordinates) =>{
-  console.log(coordinates);
-  this.setState({coordinates});
-}
-
   render() {
     return (
       <div className="App">
@@ -29,7 +10,7 @@ coordinatesHandler = (coordinates) =>{
           <h1>Weather App</h1>
         </div>
        <div className="Body">
-          <Locations location={this.state.location} coordinates={this.state.coordinates} onClick={{onLocation:this.locationHandler, onCoordinates:this.coordinatesHandler}}/>
+          <Locations/>
        </div>
       </div>
     );
